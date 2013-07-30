11760621a548a6b1f67b481aa59915e10acc4a5c
Nick Doiron <ndoiron@mapmeld.com>
2013-07-30 16:30:00 -0400

    add geojson shape script

---
+++ data/import_geojson.py
@@ -0,0 +1,34 @@
+import os, pymongo, json
+from pymongo import MongoClient
+
+mongodb_uri = 'mongodb://localhost/ncensus'
+connection = None
+if(os.environ.has_key("MONGOLAB_URI")):
+    mongodb_uri = os.environ.get("MONGOLAB_URI")
+elif(os.environ.has_key("MONGOHQ_URL")):
+    mongodb_uri = os.environ.get("MONGOHQ_URL")
+    connection = MongoClient(mongodb_uri)
+elif(os.environ.has_key("MONGODB_URI")):
+    mongodb_uri = os.environ.get("MONGODB_URI")
+
+try:
+    if(connection is None):
+        conn = mongodb_uri[ : mongodb_uri.rfind('/') + 1 ]
+        connection = pymongo.Connection( conn )
+        db = mongodb_uri[ mongodb_uri.rfind('/') + 1 : ]
+        database = connection[ db ]
+    else:
+        db = mongodb_uri[ mongodb_uri.rfind('/') + 1 : ]
+        database = connection[ db ]
+except:
+    print "MongoDB connection failed"
+    connection = None
+
+gj = json.load('blocks.geojson')
+
+for feature in gj["features"]:
+    censusid = feature["properties"]["TRACTCE10"] + "/" + feature["properties"]["BLOCKCE"]
+    
+    database.blocks.update({ "censusid": censusid }, { "shape": feature["geometry"] })
+    
+    print "mapped " + censusid
