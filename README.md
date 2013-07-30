70cb846e0050ff973669651898a22c6d816c3edd
Nick Doiron <ndoiron@mapmeld.com>
2013-07-30 13:05:55 -0400

    pull in data from census, add to MongoDB

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 heroku create neighborhood-census --stack cedar --buildpack https://github.com/monum/heroku-buildpack-python-gis.git
+
+
+## Census API
+
+Get your own API key here:
+
+http://www.census.gov/developers/tos/key_request.html
+
+Find the codes for each state using:
+
+http://api.census.gov/data/2010/sf1?key=d343614e8f46717c1ffe54bd67ae76f6bf2c9b2d&get=NAME&for=state:*
+
+Once you have the number for the state, modify this URL to list all counties in your state:
+
+http://api.census.gov/data/2010/sf1?key=d343614e8f46717c1ffe54bd67ae76f6bf2c9b2d&get=NAME&for=county:*&in=state:25
+
+Once you have the number for your county, modify these lines in app.py:
+
+    state_code = '25'
+    county_code = '025'
--- app.py
+++ app.py
@@ -1,9 +1,82 @@
+import os, sys, pymongo, requests
 from flask import Flask
 app = Flask(__name__)
+
+state_code = '25'
+county_code = '025'
+
+mongodb_uri = 'mongodb://localhost/ncensus'
+if(os.environ.has_key("MONGOLAB_URI")):
+  mongodb_uri = os.environ["MONGOLAB_URI"]
+elif(os.environ.has_key("MONGODB_URI")):
+  mongodb_uri = os.environ["MONGODB_URI"]
+
+try:
+    conn = mongodb_uri[ : mongodb_uri.rfind('/') + 1 ]
+    connection = pymongo.Connection( conn )
+    db = mongodb_uri[ mongodb_uri.rfind('/') + 1 : ]
+    database = connection[ db ]
+except:
+    print "MongoDB connection failed"
+    connection = None
 
 @app.route("/")
 def hello():
     return "Neighborhoods + Census"
+
+@app.route("/loadcounty")
+def loadcounty():
+    stats = [ "P0010001" ]
+    full_list = ""
+    tractlist = requests.get('http://api.census.gov/data/2010/sf1?key=d343614e8f46717c1ffe54bd67ae76f6bf2c9b2d&get=NAME&for=tract:*&in=state:' + state_code + '+county:' + county_code)
+    tractlist = tractlist.json()
+    first_tract = True
+    tractindex = 0
+    for tract in tractlist:
+        if(first_tract):
+            # first row is a key row
+            first_tract = False
+            tractindex = tract.index("tract")
+            continue
+        tractname = tract[tractindex]
+
+        blocklist = requests.get('http://api.census.gov/data/2010/sf1?key=d343614e8f46717c1ffe54bd67ae76f6bf2c9b2d&get=NAME,' + ','.join(stats) + '&for=block:*&in=state:' + state_code + '+county:' + county_code + '+tract:' + tractname)
+        blocklist = blocklist.json()
+
+        first_block = True
+        blockindex = 0
+        popindex = 0
+        statindexes = { }
+        for block in blocklist:
+            if(first_block):
+                # first row is a key row
+                first_block = False
+                blockindex = block.index("block")
+                for stat in stats:
+                    statindexes[ stat ] = block.index( stat )
+                continue
+            blockname = block[blockindex]
+            
+            blockstore = {
+              'censusid': tractname + "/" + blockname,
+            }
+            for stat in stats:
+              blockstore[ stat ] = block[ statindexes[stat] ]
+            database.blocks.insert(blockstore)
+            
+            full_list = full_list + tractname + "/" + blockname + "\n"
+    return full_list
+
+@app.route("/showone")
+def showone():
+    return str( database.blocks.find_one( ) )
+
+@app.route("/showall")
+def showall():
+    allblocks = ""
+    for block in database.blocks.find( ):
+        allblocks = allblocks + str(block)
+    return allblocks
 
 if __name__ == "__main__":
     app.run()
