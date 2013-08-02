e019650136ff23f39d2db67256a61c01e69f25e8
Michael Lawrence Evans <michael@codeforamerica.org>
2013-08-02 01:02:01 -0500

    Adding import configuration and cleaning up some code

--- scripts/import.py
+++ scripts/import.py
@@ -4,18 +4,11 @@ import requests
 import pymongo
 from pymongo import MongoClient
 
-if (os.environ.get('MONGOHQ_URL')):
-    MONGO_URL = os.environ.get('MONGOHQ_URL')
-    client = MongoClient(MONGO_URL)
-else:
-    host = 'localhost'
-    port = '27017'
-    client = MongoClient(host, port)
-    
-database = client.ncensus
-
-state_code = '25'
-county_code = '025'
+def load_config_json(filename):
+    with open(filename, 'rt') as f:
+        config = json.load(f)
+        
+    return config
 
 def loadcounty():
     try:
@@ -25,7 +18,8 @@ def loadcounty():
         r = 1
     stats = [ "P0010001" ]
     full_list = ""
-    tractlist = requests.get('http://api.census.gov/data/2010/sf1?key=d343614e8f46717c1ffe54bd67ae76f6bf2c9b2d&get=NAME&for=tract:*&in=state:' + state_code + '+county:' + county_code)
+    tractlist = requests.get(("http://api.census.gov/data/2010/sf1?key=%s&get=NAME&" +
+                "for=tract:*&in=state:%s+county:%s") % (API_KEY, STATE_CODE, COUNTY_CODE))
     tractlist = tractlist.json()
     first_tract = True
     tractindex = 0
@@ -35,9 +29,13 @@ def loadcounty():
             first_tract = False
             tractindex = tract.index("tract")
             continue
+        
         tractname = tract[tractindex]
+        stats_str = ','.join(stats)
 
-        blocklist = requests.get('http://api.census.gov/data/2010/sf1?key=d343614e8f46717c1ffe54bd67ae76f6bf2c9b2d&get=NAME,' + ','.join(stats) + '&for=block:*&in=state:' + state_code + '+county:' + county_code + '+tract:' + tractname)
+        blocklist = requests.get(("http://api.census.gov/data/2010/sf1?key=%s&get=NAME," +
+                     "%s&for=block:*&in=state:%s+county:%s+tract:%s")\
+                     % (API_KEY, stats_str, STATE_CODE, COUNTY_CODE, tractname))
         blocklist = blocklist.json()
 
         first_block = True
@@ -64,4 +62,20 @@ def loadcounty():
             full_list = full_list + tractname + "/" + blockname + "\n"
     return full_list
 
-loadcounty()
+if __name__ == '__main__':
+    if (os.environ.get('MONGOHQ_URL')):
+        MONGO_URL = os.environ.get('MONGOHQ_URL')
+        client = MongoClient(MONGO_URL)
+    else:
+        host = 'localhost'
+        port = 27017
+        client = MongoClient(host, port)
+        
+    database = client.ncensus
+    
+    config = load_config_json('configuration.json')
+    STATE_CODE = config["STATE_CODE"]
+    COUNTY_CODE = config["COUNTY_CODE"]
+    API_KEY = config["API_KEY"]
+    
+    loadcounty()
